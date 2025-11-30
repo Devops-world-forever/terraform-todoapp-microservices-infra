eb2645ece098c1e2b7c081aba97afc2b809b0729
AshwaniKumar22 <ashwanikumar3022.kkr@gmail.com>
2025-12-01 00:34:21 +0530

    backend-added

--- Environments/Dev/provider.tf
+++ Environments/Dev/provider.tf
@@ -1,14 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.52.0"
     }
-  }
+  }  
+     backend "azurerm" {
+  resource_group_name  = "githubaction-rg"
+  storage_account_name = "githubactionstrg1"
+  container_name       = "tfstate"
+  key                  = "dev.tfstate"
+}
 }
 
 provider "azurerm" {
-    features {}
+  features {}
   # Configuration options
   subscription_id = "f63f9a72-32ba-4cf2-acf0-446dad259c1b"
 }
--- Modules/azurerm_storage_account/main.tf
+++ Modules/azurerm_storage_account/main.tf
@@ -4,8 +4,8 @@ resource "azurerm_storage_account" "storage_account" {
   name                     = each.value.storage_name
   resource_group_name      = each.value.rg_name
   location                 = each.value.location
-  account_tier             = each.value.account_tier "Standard"
-  account_replication_type = each.value.account_replication_type "GRS"
+  account_tier             = each.value.account_tier 
+  account_replication_type = each.value.account_replication_type 
 
 tags     = merge(var.common_tags, each.value.tags)
 }
